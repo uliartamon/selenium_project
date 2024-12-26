e859eb0acad2db911f613db096991cb179524261
Ulyana <uliartamon@gmail.com>
2024-12-26 12:29:03 +0300

    add default language

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None, help='Choose language')
+    parser.addoption('--language', action='store', default='ru', help='Choose language')
 
 @pytest.fixture(scope='function')
 def browser(request):
