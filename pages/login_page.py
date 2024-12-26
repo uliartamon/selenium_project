3364671821611c3e5042d6ee5926824eba4e856f
Ulyana <uliartamon@gmail.com>
2024-12-26 12:20:29 +0300

    add login page check

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from base_page import BasePage
+from locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        
+        assert 'login' in self.browser.current_url, 'Parameter "login" was not found in the current link.'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form was not found in the current link.'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), 'Registration form was not found in the current link.'
---
+++ tests/test_login_page.py
@@ -0,0 +1,3 @@
+from pages.login_page import LoginPage
+
+URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
